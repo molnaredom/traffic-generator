bb9e0d4ccb493ebad93222cec87b8f1743c88b49
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-06-04 13:57:36 +0200

    UPDATE: replace threads with concurrent.futures

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-
+import time
 import subprocess
-import platform
-import threading
+import concurrent.futures
 
-def ping(host="google.com"):
-    param = '-n' if platform.system().lower() == "windows" else '-c'
-    command = ['ping', '-c', '10', host]
+
+def ping(pingable_host, size, time_intervall_between_pings, ping_count):
+    command = [
+        'ping',
+        '-q'
+        '-c', str(ping_count),
+        # '-s', str(size), todo
+        # '-i', time_intervall_between_pings,
+        pingable_host
+    ]
     return subprocess.call(command)
 
 
 host = "google.com"
+ping_count = 4
+threads = 50
+size = 200
+time_intervall_between_pings = 100
 
-for _ in range(10):
-    t = threading.Thread(target=ping, args=[host])
-    t.start()
+start = time.perf_counter()
 
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for _ in range(threads):
+        executor.submit(ping, host, size, time_intervall_between_pings, ping_count)
+
+end = time.perf_counter()
+
+print("DURATION:", round(end - start, 4), "second(s)")
