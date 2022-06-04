e58a6b1b3923666cde531cd52e0ed513ddbc3f54
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-06-04 12:58:36 +0200

    UPDATE: pinging in multiple threads

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 
 import subprocess
 import platform
+import threading
 
-def ping(host):
+def ping(host="google.com"):
     param = '-n' if platform.system().lower() == "windows" else '-c'
     command = ['ping', '-c', '10', host]
     return subprocess.call(command)
 
 
 host = "google.com"
-print(ping(host))
+
+for _ in range(10):
+    t = threading.Thread(target=ping, args=[host])
+    t.start()
 
